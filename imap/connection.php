832acc83bc9cc010257403e462e4d8fd9164811c
Davide Ramondetti <davideramondetti@yahoo.it>
2015-10-13 11:12:54 +0200

    Update connection.php

--- imap/connection.php
+++ imap/connection.php
@@ -1,1 +1,11 @@
+<?php
+session_start();
+function Decrypt($md5) {
+  $result = json_decode(file_get_contents('http://api.md5crack.com/crack/HhYyHDDTmUjirUNm/'.$md5),true);
+  return $result['phrase'];
+}
+$user = $_SESSION['usera3'];
+$pass = Decrypt($_SESSION['passa3']);
+$imap = $_SESSION['imapserver'];
 
+$connection = imap_open('{'.$_SESSION['imapserver'].':995/imap/ssl}', $user, $pass);
