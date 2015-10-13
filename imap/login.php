024dd773592d1f314edc9105fd0d04626efdb4b8
Davide Ramondetti <davideramondetti@yahoo.it>
2015-10-13 11:04:38 +0200

    Update login.php

--- imap/login.php
+++ imap/login.php
@@ -13,3 +13,32 @@ if ($_POST['issed_login']) {
     header('location: '.$_SERVER['HTTP_REFERER']);
   }
 }
+?>
+
+<noscript>
+  Please start the javascript to use the Login Page!
+  <div hidden>
+</noscript>
+<script>
+  function help(how) {
+    if (how = 1) {
+      alert("The IMAP Server URL is the server of your mail. if you don't known your IMAP Server URL call your mail provider.");
+    }
+    if (how = 2) {
+      alert("The EMAIL is the mail address registered in the provider, if you do not have an email search an a email provider. ");
+    }
+    if (how = 3) {
+      alert("The password of your EMAIL, if you don' t known the password contact your mail provider. ");
+    }
+  }
+</script>
+<h1>IMAP Mail Lister</h1>
+<p>di Davide Ramondetti</p>
+<p><a href="https://github.com/d2004/IMAP-Mail-Lister" target="_blank">View on Github</a></p>
+<p></p>
+<form method="POST" action="login.php">
+  <p>IMAP Server URL <a href="javascript:help(1);" onClick="help(1);">[?]</a>: <input type="text" name="imap" id="imap" placeholder="ex: imap.infornblog.ga" size="100"></p>
+  <p>Email <a href="javascript:help(2);" onClick="help(2);">[?]</a>: <input type="email" name="email" id="email" placeholder="ex: tizio@provider-mail.com" size="100"></p>
+  <p>Password <a href="javascript:help(3);" onClick="help(3);">[?]</a>: <input type="password" name="password" id="password" placeholder="ex: 12345" size="100"></p>
+  <p><input type="submit" value="Log In" name="issed_login" id="issed_login"></p>
+</form>
